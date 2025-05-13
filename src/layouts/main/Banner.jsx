373f9f9a465e44cbe2298ce0b8ee428b0c1987ba
mazhar <mazharopal@gmail.com>
2025-05-13 15:31:39 +0500

    deploying

--- src/layouts/main/Banner.jsx
+++ src/layouts/main/Banner.jsx
@@ -6,8 +6,14 @@ export default function Banenr() {
     <>
     <div className="container full-container">
       <div className="row g-0">
-        <div className="banner">
-          <img className='img z-n1' src={BannerImg} alt="architecture" />
+        <div className="banner"  style={{
+                    backgroundImage: `url(${BannerImg})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    height: "400px",
+                }}>
+          {/* <img className='img z-n1' src={BannerImg} alt="architecture" /> */}
           <p className='z-3 '>THE EASY, RELIABLE WAY <br />
             TO TAKE CARE OF YOUR  HOME</p>
         </div>
