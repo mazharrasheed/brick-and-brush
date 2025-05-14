018212f628f6fbfdb62e8a3b0f5a5d0865323cb9
mazhar <mazharopal@gmail.com>
2025-05-14 17:13:03 +0500

    services detail added

--- src/components/ServiceDetail.jsx
+++ src/components/ServiceDetail.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import image from '../assets/images/service-1.svg'
+import { Link,} from 'react-router-dom'
 const images = import.meta.glob('../assets/images/*.svg', { eager: true });
 console.log(image)
 
@@ -15,6 +16,10 @@ export default function ServiceDetail() {
             <p>You are viewing details for service ID: {id}</p>
             {/* Example: Load image and description here */}
             <img src={image.default} alt={`Service ${id}`} />
+            <h1 className='mt-5'>
+                <Link className={`nav-link me-2 fs-6 ${location.pathname === "/services/" ? "active " : ""}`} aria-current="page" to="/services/">Back to Services</Link>
+            </h1>
         </div>
+        
     );
 }
