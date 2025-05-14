04afba8a89c70e2c20ab18df301cc0be75a6c978
mazharrasheed <mazharopal@gmail.com>
2025-05-14 21:47:46 +0500

    deploying

--- src/components/ServiceDetail.jsx
+++ src/components/ServiceDetail.jsx
@@ -1,6 +1,14 @@
 import { useParams } from 'react-router-dom';
 import image from '../assets/images/service-1.svg'
-import { Link,} from 'react-router-dom'
+// import { Link, } from 'react-router-dom'
+
+import '../assets/css/servicedetail.css'
+import BannerSm from './BannerSm'
+import ContactForm from './ContactForm';
+import smallBannerImg from '../assets/images/architecture.svg';
+import headerImg from '../assets/images/living-room.png';
+import PageHeader from './PageHeader'
+
 const images = import.meta.glob('../assets/images/*.svg', { eager: true });
 console.log(image)
 
@@ -9,17 +17,49 @@ export default function ServiceDetail() {
     const imagePath = `../assets/images/service-${id}.svg`;
     const image = images[imagePath];
 
+    const text1 = 'DECK PAINTING, STAINING'
+    const text2 = 'THE EASY RELIABLE WAY'
+    const text3 = 'TO TAKE CARE OF YOU HOME'
+    const text4 = 'YOUR RENOVATION WILL RUN QUAILITAIVELY'
+    const text5 = 'THANKS TO OUR 5 STEP PROCESS'
+
     // You could use `id` to fetch detail from backend or use a local data array
     return (
-        <div className="container mt-5 mb-5">
-            <h1>Service Details</h1>
-            <p>You are viewing details for service ID: {id}</p>
-            {/* Example: Load image and description here */}
-            <img src={image.default} alt={`Service ${id}`} />
-            <h1 className='mt-5'>
-                <Link className={`nav-link me-2 fs-6 ${location.pathname === "/services/" ? "active " : ""}`} aria-current="page" to="/services/">Back to Services</Link>
-            </h1>
-        </div>
-        
+        <>
+<div className='mb-5'>
+<PageHeader image={headerImg} text1={text1} text2={text2} text3={text3}></PageHeader>
+</div>
+    
+
+            <div className='row  service-detail   '>
+                <h1 className='mb-5'>Service Details</h1>
+                <div className='col-md-6'>
+                    {/* Example: Load image and description here */}
+                    <img className='detail-img' src={image.default} alt={`Service ${id}`} />
+                    {/* <h1 className='mt-5'>
+                        <Link className={`nav-link me-2 fs-6 ${location.pathname === "/services/" ? "active " : ""}`} aria-current="page" to="/services/">Back to Services</Link>
+                    </h1> */}
+                </div>
+                <div className='col-md-6 d-flex justify-content-center align-items-center'>
+
+                    <p className='fs-1 '>
+                    On the other hand, the framework and place of personnel training require us to analyze the personnel training system, it corresponds to pressing needs. Likewise, the beginning of daily work on the formation of a position requires us to analyze the appropriate 
+                    On the other hand, the framework and place of personnel training require us to analyze the personnel training system, it corresponds to pressing needs. Likewise, the beginning of daily work on the formation of a position requires us to analyze the appropriate 
+                    </p>
+                </div>
+
+            </div>
+
+            <div className='mt-5 mb-5' >
+
+                <BannerSm image={smallBannerImg} text1={text4} text2={text5}></BannerSm>
+            </div>
+
+
+            <div>
+                <ContactForm></ContactForm>
+            </div>
+        </>
+
     );
 }
